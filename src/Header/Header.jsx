79f790d11965bf1f3b08f7c5c5032094d7a7e6bb
Alireza GT4X <alirezagt4x@gmail.com>
2026-02-20 00:57:38 +0330

    Delete src directory

--- src/Header/Header.jsx
+++
@@ -1,9 +0,0 @@
-import Navbar from "./Navbar";
-
-export default function Header() {
-	return (
-		<div className="bg-linear-to-r from-blue-800 to-sky-400">
-			<Navbar />
-		</div>
-	);
-}
--- src/Header/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-
-export default function Logo() {
-	return (
-		<Link to={"/"}>
-			<img src="/public/EramLogo.webp" alt="Logo" className="w-40 lg:w-48 cursor-pointer" />
-		</Link>
-	);
-}
