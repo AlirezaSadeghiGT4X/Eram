ad20c5eecd757f6025fd317d900acaa3cf2126b4
Alireza GT4X <alirezagt4x@gmail.com>
2026-02-26 02:21:01 +0330

    Delete src directory

--- src/DownloadWinApp/Banner.jsx
+++
@@ -1,7 +0,0 @@
-export default function Banner() {
-	return (
-		<div className="w-full">
-            <img src="/Images/EramDownloadAppBanner.png" alt="Download Windows Application" className="w-full h-120" />
-        </div>
-	);
-}
--- src/Textbook/Banner.jsx
+++
@@ -1,7 +0,0 @@
-export default function Banner() {
-	return (
-		<div className="w-full">
-            <img src="/Images/EramTextbookBanner.png" alt="Eram Textbook" className="w-full xl:h-120" />
-        </div>
-	);
-}
