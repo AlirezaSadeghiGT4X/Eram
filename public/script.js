2fd15895df0cccd0eb22a31bd2809306d12d05f4
Alireza Sadeghi <alirezagt4x@gmail.com>
2026-07-02 21:21:05 +0330

    Delete script.js file for no bug on deploy

--- public/script.js
+++
@@ -1,6 +0,0 @@
-function DropdownHandler() {
-    if (localStorage.getItem("svgClicked") != "true") {
-        localStorage.setItem("dropdown", "none")
-    }
-    localStorage.setItem("svgClicked", "false")
-}
